c1e508ff166cbb6e2ea3ccf98761cb511b0e98b2
sumeetgaikwad48-code <sumeetgaikwad48@gmail.com>
2026-01-16 12:26:40 +0530

    Added Variables and Data Types with Examples

---
+++ 01_basics&variables.py
@@ -0,0 +1,36 @@
+#This is my first Python program
+print("Hello World")
+print("I like to Box")
+#Variables ---> (string, integer, float, boolean)
+
+#Strings
+first_name = "Mitsu"
+food = "Burger"
+email = "Mitsu123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+#Integers
+age = 18
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} Tomatoes")
+print(f"Your class has {num_of_students} students in class")
+
+#Floats
+price = 10.56
+distance = 5.5
+weight = 66.5
+
+print(f"The price is ₹{price}")
+print(f"You ran {distance}KM")
+print(f"Your weight is {weight}kgs")
+
+#Booleans
+is_student = True
+
+print(f"Are you a student?: {is_student}")
