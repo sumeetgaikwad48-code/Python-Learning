d77781dba4f8d9646637419ae6dc79178cc485e2
sumeetgaikwad48-code <sumeetgaikwad48@gmail.com>
2026-01-16 13:26:18 +0530

    02-user-input.py

--- 02-user-input.py
+++ 02-user-input.py
@@ -7,3 +7,19 @@ age +=1
 print(f"Hello {name}!")
 print("Happy Birthday!")
 print(f"You are {age} years old.")
+
+#Example 1 for User-Input
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = length * width
+print(f"The area is {area}cm")
+
+#Example 2 for User-Input
+
+item = input("What item would like to buy?: ")
+price = int(input("What is the price? :"))
+quantity = int(input("How many items would you like to buy?: "))
+total = price * quantity
+
+print(f"The total is ₹{total}")
