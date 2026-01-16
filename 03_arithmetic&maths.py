af6365d44282ab8fd96ccf29ba0d1186524e3c72
sumeetgaikwad48-code <sumeetgaikwad48@gmail.com>
2026-01-16 13:49:34 +0530

    Added Arithmetic & Maths

---
+++ 03_arithmetic&maths.py
@@ -0,0 +1,27 @@
+friends = 0
+
+#Addition
+#friends = friends + 1
+#friends += 1
+
+#Subtraction
+#friends = friends - 1
+#friends -= 1
+
+#Multiplication
+#friends = friends * 1
+#friends *= 1
+
+#Division
+#friends = friends / 1
+#friends /= 1
+
+#Exponention
+#friends = friends ** 2
+#friends **= 2
+
+#Modulus ---> (Gives you remainder)
+#friends = friends % 3
+#friends %= 3
+
+print(friends)
